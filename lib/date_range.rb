fee13c4829c196f7c35b00e49cae8a014f441be2
Len Smith <ignu.smith@gmail.com>
2010-06-30 14:40:44 -0400

    made daterange a module

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,12 +1,12 @@
-class DateRange < Range
-  
-  def initialize(date1, date2)
-    @start_date, @end_date = date1, date2
-    super(date1, date2)
-  end
+module DateRange 
 
   def months
     (Month.new(@start_date)..Month.new(@end_date))
   end
 
+  def days
+    (@start_date)..(@end_date)
+  end
+
 end
+
--- spec/date_range_spec.rb
+++ spec/date_range_spec.rb
@@ -1,15 +1,23 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+class Campaign
+  include DateRange
+
+  def initialize(date1, date2)
+    @start_date, @end_date = date1, date2
+  end
+end
+
 describe "DateRange" do
 
-  let(:range) { DateRange.new(DateTime.new(2001,1,1), DateTime.new(2001,7,5))}
+  let(:campaign) { Campaign.new(DateTime.new(2001,1,1), DateTime.new(2001,7,5))}
 
   it "should count the days in the range" do
-    range.to_a.length.should == 31+28+31+30+31+30+5
+    campaign.days.to_a.length.should == 31+28+31+30+31+30+5
   end
  
   it "should be able to get the number of months in a range" do
-    range.months.to_a.length.should == 7
+    campaign.months.to_a.length.should == 7
   end
 
 end
