6254c5cb056b327300f0413ad9e1c71844e30dcf
Len Smith <ignu.smith@gmail.com>
2010-06-30 19:01:12 -0400

    added date range overlapping detection

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -9,10 +9,38 @@ module DateRange
   end
  
   def encompasses(other)
-    _start < other.start_date && _end > other.end_date 
+    _start <= other.start_date && _end >= other.end_date 
+  end
+
+  def intersects(other) 
+    other.encompasses(self) || encompasses(other) || overlaps(other)
+  end
+
+  def & (other)
+    return [] unless intersects other
+    return other if encompasses(other)
+    return self if other.encompasses(self)
+    return SimpleRange.new(other.start_date, _end) if overlaps_left_side(other)
+    SimpleRange.new(_start, other.end_date)
+  end
+
+  def same_range_as(other) 
+    other.start_date == _start && other.end_date == _end
   end
 
   private
+   
+  def overlaps(other)
+    overlaps_left_side(other) || overlaps_right_side(other)
+  end
+
+  def overlaps_right_side(other)
+    other.start_date <= _start && other.end_date >= _start
+  end
+
+  def overlaps_left_side(other)
+    other.start_date <= _end && other.end_date >= _end
+  end
 
   def _start
     @start_date ||= start_date
@@ -23,3 +51,12 @@ module DateRange
   end
 end
 
+class SimpleRange
+  include DateRange
+
+  attr_accessor :start_date, :end_date
+
+  def initialize(date1, date2)
+    @start_date, @end_date = date1, date2
+  end
+end
--- spec/date_range_spec.rb
+++ spec/date_range_spec.rb
@@ -1,41 +1,61 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-class Campaign
-  include DateRange
-
-  attr_accessor :start_date, :end_date
-
-  def initialize(date1, date2)
-    @start_date, @end_date = date1, date2
-  end
-end
-
 describe "DateRange" do
 
   let(:firstOfYear)  { DateTime.new(2001,1,1) }
   let(:endOfYear)    { DateTime.new(2001,12,31) }
   let(:firstOfMarch) { DateTime.new(2001,3,1) }
   let(:endOfMarch)   { DateTime.new(2001,3,30) }
-  let(:firstOfApril) { DateTime.new(2001,4,1) }
-  let(:endOfApril)   { DateTime.new(2001,4,30) }
-  let(:campaign)     { Campaign.new(DateTime.new(2001,1,1), DateTime.new(2001,7,5))}
-  let(:year)         { Campaign.new(firstOfYear, endOfYear)}
-  let(:march)        { Campaign.new(firstOfMarch, endOfMarch)}
-  let(:april)        { Campaign.new(firstOfApril, endOfApril)}
+  let(:firstOfJuly) { DateTime.new(2001,7,1) }
+  let(:endOfJuly)   { DateTime.new(2001,7,31) }
+  let(:range)     { SimpleRange.new(DateTime.new(2001,1,1), DateTime.new(2001,7,5))}
+  let(:year)         { SimpleRange.new(firstOfYear, endOfYear)}
+  let(:march)        { SimpleRange.new(firstOfMarch, endOfMarch)}
+  let(:july)         { SimpleRange.new(firstOfJuly, endOfJuly)}
+
+  it "should be equal if start and end dates are the same" do 
+    year.same_range_as(year).should == true 
+  end
 
   it "should count the days in the range" do
-    campaign.days.to_a.length.should == 31+28+31+30+31+30+5
+    range.days.to_a.length.should == 31+28+31+30+31+30+5
   end
  
   it "should be able to get the number of months in a range" do
-    campaign.months.to_a.length.should == 7
+    range.months.to_a.length.should == 7
   end
 
   describe DateRange, "intersecting dates" do 
-    it "should tell if one date encompasses another" do  
+
+    it "should tell if one date range encompasses another" do  
       year.encompasses(march).should == true 
       march.encompasses(year).should == false
-      march.encompasses(april).should == false
+      march.encompasses(july).should == false
     end 
+
+    it "should be able to tell if one date range intersects another" do 
+      year.intersects(march).should == true
+      year.intersects(year).should == true
+      march.intersects(year).should == true
+      march.intersects(july).should == false
+      range.intersects(july).should == true
+      july.intersects(range).should == true
+    end
+
+    it "can return the intersection" do 
+      (year & march).should == march
+      (march & year).should == march
+      (march & march).should == march
+      
+      left = (range & july)
+      left.start_date.should == firstOfJuly
+      left.end_date.should == range.end_date
+      left = (july & range)
+      left.start_date.should == firstOfJuly
+      left.end_date.should == range.end_date
+      
+      (march & july).should == []
+    end
+
   end
 end
