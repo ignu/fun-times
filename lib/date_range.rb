cd77a5f0ca26c9eb6abd68204cd3e81f583de7b8
Len Smith <ignu.smith@gmail.com>
2010-06-30 17:59:37 -0400

    added encompasses

--- lib/date_range.rb
+++ lib/date_range.rb
@@ -1,12 +1,25 @@
 module DateRange 
 
   def months
-    (Month.new(@start_date)..Month.new(@end_date))
+    (Month.new(_start)..Month.new(_end))
   end
 
   def days
-    (@start_date)..(@end_date)
+    (_start)..(_end)
+  end
+ 
+  def encompasses(other)
+    _start < other.start_date && _end > other.end_date 
   end
 
+  private
+
+  def _start
+    @start_date ||= start_date
+  end
+
+  def _end 
+    @end_date ||= end_date
+  end
 end
 
--- spec/date_range_spec.rb
+++ spec/date_range_spec.rb
@@ -3,6 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 class Campaign
   include DateRange
 
+  attr_accessor :start_date, :end_date
+
   def initialize(date1, date2)
     @start_date, @end_date = date1, date2
   end
@@ -10,7 +12,16 @@ end
 
 describe "DateRange" do
 
-  let(:campaign) { Campaign.new(DateTime.new(2001,1,1), DateTime.new(2001,7,5))}
+  let(:firstOfYear)  { DateTime.new(2001,1,1) }
+  let(:endOfYear)    { DateTime.new(2001,12,31) }
+  let(:firstOfMarch) { DateTime.new(2001,3,1) }
+  let(:endOfMarch)   { DateTime.new(2001,3,30) }
+  let(:firstOfApril) { DateTime.new(2001,4,1) }
+  let(:endOfApril)   { DateTime.new(2001,4,30) }
+  let(:campaign)     { Campaign.new(DateTime.new(2001,1,1), DateTime.new(2001,7,5))}
+  let(:year)         { Campaign.new(firstOfYear, endOfYear)}
+  let(:march)        { Campaign.new(firstOfMarch, endOfMarch)}
+  let(:april)        { Campaign.new(firstOfApril, endOfApril)}
 
   it "should count the days in the range" do
     campaign.days.to_a.length.should == 31+28+31+30+31+30+5
@@ -20,4 +31,11 @@ describe "DateRange" do
     campaign.months.to_a.length.should == 7
   end
 
+  describe DateRange, "intersecting dates" do 
+    it "should tell if one date encompasses another" do  
+      year.encompasses(march).should == true 
+      march.encompasses(year).should == false
+      march.encompasses(april).should == false
+    end 
+  end
 end
