08bef00266f90f3857c025ad96986ce4411f1787
Len Smith <ignu.smith@gmail.com>
2010-07-02 12:37:28 -0400

    added start date

--- lib/quarter.rb
+++ lib/quarter.rb
@@ -1,11 +1,14 @@
 class Quarter
-  attr_accessor :number, :year
+  attr_accessor :number, :year, :start_date, :end_date
 
   def initialize(q,y) 
+    months = [1, 4, 7, 10]
     @number, @year = q, y
+    @start_date = DateTime.new(2009, months[q-1], 1)
   end
 
   def self.from(date)
     new(date.month/4+1, date.year)
   end
+
 end
--- spec/quarter_spec.rb
+++ spec/quarter_spec.rb
@@ -13,4 +13,9 @@ describe "Quarter" do
     quarter.year.should == 2009
   end
 
+  it "sets the start date correctly" do 
+    Quarter.new(1, 2009).start_date.should == DateTime.new(2009, 1, 1)
+    Quarter.new(2, 2009).start_date.should == DateTime.new(2009, 4, 1)
+  end
+
 end
