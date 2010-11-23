a765ff8809ccbd3d5c94bc6f58530ec8d210c707
Xiao Yu <xiao@yu.name>
2010-11-22 21:34:05 -0500

    Adding spec for finding the union of DateRange
    When dates overlap the returned range should be the combination of
    both DateRange. When the ranges do not overlap [] is returned akin
    to the intersect (&) operator.

--- spec/date_range_spec.rb
+++ spec/date_range_spec.rb
@@ -13,11 +13,11 @@ describe "DateRange" do
   let(:march)        { SimpleRange.new(firstOfMarch, endOfMarch)}
   let(:july)         { SimpleRange.new(firstOfJuly, endOfJuly)}
 
-  it "should be equal if start and end dates are the same" do 
-    year.same_range_as(year).should == true 
+  it "should be equal if start and end dates are the same" do
+    year.same_range_as(year).should == true
   end
 
-  it "should have the quarters in a range" do 
+  it "should have the quarters in a range" do
     year.quarters.first.number.should == 1
     year.quarters.to_a.length.should == 4
   end
@@ -25,20 +25,20 @@ describe "DateRange" do
   it "should count the days in the range" do
     range.days.to_a.length.should == 31+28+31+30+31+30+5
   end
- 
+
   it "should be able to get the number of months in a range" do
     range.months.to_a.length.should == 7
   end
 
-  describe DateRange, "intersecting dates" do 
+  describe DateRange, "intersecting dates" do
 
-    it "should tell if one date range encompasses another" do  
-      year.encompasses(march).should == true 
+    it "should tell if one date range encompasses another" do
+      year.encompasses(march).should == true
       march.encompasses(year).should == false
       march.encompasses(july).should == false
-    end 
+    end
 
-    it "should be able to tell if one date range intersects another" do 
+    it "should be able to tell if one date range intersects another" do
       year.intersects(march).should == true
       year.intersects(year).should == true
       march.intersects(year).should == true
@@ -47,28 +47,42 @@ describe "DateRange" do
       july.intersects(range).should == true
     end
 
-    it "can return the intersection" do 
+    it "can return the intersection" do
       (year & march).should == march
       (march & year).should == march
       (march & march).should == march
-      
+
       left = (range & july)
       left.start_date.should == firstOfJuly
       left.end_date.should == range.end_date
       left = (july & range)
       left.start_date.should == firstOfJuly
       left.end_date.should == range.end_date
-      
+
       (march & july).should == []
     end
 
+    it "can return the union" do
+      (year | march).should == year
+      (march | year).should == year
+      (march | march).should == march
+
+      union = (range | july)
+      union.start_date.should == range.start_date
+      union.end_date.should == endOfJuly
+      union = (july | range)
+      union.start_date.should == range.start_date
+      union.end_date.should == endOfJuly
+
+      (march | july).should == []
+    end
 
     it "can figure out the number of days in" do
       year.number_of_days_in(march).should == 30
       march.number_of_days_in(july).should == 0
     end
 
-    it "returns null collections when start or end date is not set" do 
+    it "returns null collections when start or end date is not set" do
       r = SimpleRange.new(nil, DateTime.new)
       r.months.length.should == 0
       r = SimpleRange.new(DateTime.new, nil)
